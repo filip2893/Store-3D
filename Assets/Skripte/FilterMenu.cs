7251bb09b7170506a66f8dc8207b26c1ee54255a
filip2893 <filip2893@gmail.com>
2019-01-30 19:34:10 +0100

    project 1.0

---
+++ Assets/Skripte/FilterMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Data;
+
+public class FilterMenu : MonoBehaviour {
+	public GameObject player;
+	public Dropdown dropdown;
+	public InputField input;
+
+	private static GameObject [] entities;
+	// Use this for initialization
+	void Start () {
+		entities = GameObject.FindGameObjectsWithTag("entity");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnEnable(){
+		player.SetActive (false);
+	}
+	void OnDisable(){
+		player.SetActive (true);
+	}
+
+	public void search(){
+		getEntities ();
+	}
+
+	public void showAllEntities(){
+		foreach (GameObject en in entities) {
+			en.SetActive (true);
+		}
+		this.gameObject.SetActive (false);
+	}
+
+	void getEntities(){	
+		bool show = false;
+		if (dropdown.value == 0) {
+			foreach (GameObject en in entities) {
+				show = comparator (en.GetComponent<Entity> ().sifra, input.text.ToString ());
+				en.SetActive (show);
+			}
+		}else if(dropdown.value == 1){
+			foreach (GameObject en in entities) {
+				show = comparator (en.GetComponent<Entity> ().name, input.text.ToString ());
+				en.SetActive (show);
+			}					
+		}
+		this.gameObject.SetActive (false);
+	}
+
+	private bool comparator(string val1, string val2){
+		if (val1 == val2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ Assets/Skripte/StartMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Data;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	public Dropdown dropdown;
+
+
+	// Use this for initialization
+	void Start () {
+		List<string> prostori = new List<string> ();
+
+		dropdown.ClearOptions ();
+		DatabaseConnection database = DatabaseConnection.getInstance ();
+		DataTable data = database.Query ("Select Naziv from Prostori;");
+
+		foreach(DataRow r in data.Rows){
+			prostori.Add (r["Naziv"].ToString());
+		}
+		dropdown.AddOptions (prostori);
+	}
+	// Update is called once per frame
+	void Update () {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+	public void OnOptionSelected(){
+		int sceneIndex = dropdown.value + 1;
+		SceneManager.LoadScene (sceneIndex);
+	}
+	public void closeApplication(){
+		Application.Quit ();
+	}
+}
